cab728e5af3bc56119154e635275fee555bd33d5
Anushka Shanker <104841806+AnushkaShanker@users.noreply.github.com>
2025-06-27 11:08:52 +0530

    Update User.java

--- src/entity/User.java
+++ src/entity/User.java
@@ -2,30 +2,66 @@ package entity;
 import java.util.*;
 public class User {
     private int userId;
-    private String username;
+    private String password;
+    private String phone;
+    private String shippingAddress;
+    private String billingAddress;
+    private List<Order> orders = new ArrayList<>();
+    private String name;
     private String email;
-    private List<CartItem> cart;
 
-    public User(int userId, String username, String email, List<CartItem> cart) {
-        this.userId = userId;
-        this.username = username;
-        this.email = email;
-        this.cart = cart;
+    public String getName() {
+        return name;
     }
-
-    public int getUserId() {
-        return userId;
+    public void setName(String name) {
+        this.name = name;
     }
-
-    public String getUsername() {
-        return username;
-    }
-
     public String getEmail() {
         return email;
     }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public int getUserId() {
+        return userId;
+    }
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public String getShippingAddress() {
+        return shippingAddress;
+    }
+    public void setShippingAddress(String shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+    public String getBillingAddress() {
+        return billingAddress;
+    }
+    public void setBillingAddress(String billingAddress) {
+        this.billingAddress = billingAddress;
+    }
+    public void addOrder(Order order) {
+        orders.add(order);
+    }
 
-    public List<CartItem> getCart() {
-        return cart;
+    public void printOrders() {
+        System.out.println("Orders for user: " + getName());
+        for (Order o : orders) {
+            System.out.println("Order ID: " + o.getOrderId() + ", Total: " + o.getTotalAmount());
+        }
     }
 }
+
+   
