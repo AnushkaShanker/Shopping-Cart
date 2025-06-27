57b5ead113950b8a7e51ea58b57eb9bd7d46e5e8
Anushka Shanker <104841806+AnushkaShanker@users.noreply.github.com>
2025-06-27 10:26:58 +0530

    Update Admin.java

--- src/entity/Admin.java
+++ src/entity/Admin.java
@@ -1,32 +1,13 @@
 package entity;
-public class Admin{
-    private int adminId;
-    private String password;
+ private int adminId;
     private String name;
-    private String email;
 
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
+    public Admin(int adminId, String name) {
+        this.adminId = adminId;
         this.name = name;
     }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public int getAdminId() {
-        return adminId;
-    }
-    public void setAdminId(int adminId) {
-        this.adminId = adminId;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
+
+    public void manageProduct(Product product) {
+        System.out.println("Managing product: " + product.getName());
     }
 }
