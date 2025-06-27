3b7245d17137212e2de9b6df7f6cdee9d555e212
Anushka Shanker <104841806+AnushkaShanker@users.noreply.github.com>
2025-06-27 11:13:11 +0530

    Update CartItem.java

--- src/entity/CartItem.java
+++ src/entity/CartItem.java
@@ -1,19 +1,33 @@
 package entity;
+package entity;
+public class Admin{
+    private int adminId;
+    private String password;
+    private String name;
+    private String email;
 
-public class CartItem {
-	  private Product product;
-    private int quantity;
-
-    public CartItem(Product product, int quantity) {
-        this.product = product;
-        this.quantity = quantity;
+    public String getName() {
+        return name;
     }
-
-    public Product getProduct() {
-        return product;
+    public void setName(String name) {
+        this.name = name;
     }
-
-    public int getQuantity() {
-        return quantity;
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public int getAdminId() {
+        return adminId;
+    }
+    public void setAdminId(int adminId) {
+        this.adminId = adminId;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
