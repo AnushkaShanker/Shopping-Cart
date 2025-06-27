57eaba2a6526522f94104919ea967c2206fb9c81
Anushka Shanker <104841806+AnushkaShanker@users.noreply.github.com>
2025-06-27 10:30:39 +0530

    Update OrderItem.java

--- src/entity/OrderItem.java
+++ src/entity/OrderItem.java
@@ -1,39 +1,15 @@
 package entity;
 
 public class OrderItem {
-	 private int orderItemId;
-	    private int orderId;
-	    private Product product;
-	    private int quantity;
+    private Product product;
+    private int quantity;
 
-	    // Getters and Setters
-	    public int getOrderItemId() {
-	        return orderItemId;
-	    }
-	    public void setOrderItemId(int orderItemId) {
-	        this.orderItemId = orderItemId;
-	    }
-	    public int getOrderId() {
-	        return orderId;
-	    }
-	    public void setOrderId(int orderId) {
-	        this.orderId = orderId;
-	    }
-	    public int getQuantity() {
-	        return quantity;
-	    }
-	    public void setQuantity(int quantity) {
-	        this.quantity = quantity;
-	    }
-	    public Product getProduct() {
-	        return product;
-	    }
-	    public void setProduct(Product product) {
-	        this.product = product;
-	    }
+    public OrderItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public double getTotalPrice() {
+        return product.getPrice() * quantity;
+    }
 }
-
-
-
-
-	
