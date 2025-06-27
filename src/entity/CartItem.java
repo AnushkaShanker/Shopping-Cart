ade8592af75032e23f9b6ec90e1a522c176c072b
Anushka Shanker <104841806+AnushkaShanker@users.noreply.github.com>
2025-06-27 10:29:11 +0530

    Update CartItem.java

--- src/entity/CartItem.java
+++ src/entity/CartItem.java
@@ -1,41 +1,19 @@
 package entity;
 
 public class CartItem {
-	 private int cartItemId;
-	    private int cartId;
-	    private Product product;
-	    private int quantity;
+	  private Product product;
+    private int quantity;
 
-	    // Getters and Setters
-	    public int getCartItemId() {
-	        return cartItemId;
-	    }
-	    public void setCartItemId(int cartItemId) {
-	        this.cartItemId = cartItemId;
-	    }
-	    public int getCartId() {
-	        return cartId;
-	    }
-	    public void setCartId(int cartId) {
-	        this.cartId = cartId;
-	    }
-	
-	    public int getQuantity() {
-	        return quantity;
-	    }
-	    public void setQuantity(int quantity) {
-	        this.quantity = quantity;
-	    }
-	    public Product getProduct() {
-	        return product;
-	    }
-	    public void setProduct(Product product) {
-	        this.product = product;
-	    }
-	    public void printCartItem() {
-	        System.out.println("CartItem ID: " + cartItemId + ", Product ID: " + product.getProductId() + ", Product Name: " + product.getName() +
-	                ", Price: " + product.getPrice() + ", Quantity: " + quantity);
-	    }
+    public CartItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
 }
-
-
